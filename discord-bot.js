53255aa7526eb7120505213f6af3646becd84ea1
Benjamin Leuckefeld <code@deratrox.de>
2016-12-21 19:53:14 +0100

    added shoosh

--- discord-bot.js
+++ discord-bot.js
@@ -613,7 +613,8 @@ const sounds = {
     'awesome': 'Rainbow Dash/so awesome.mp3',
     'louder': 'Rainbow Dash/louder.mp3',
     'trixie': 'Trixie/the g and p t.mp3',
-    'swear': 'Pinkie Pie/pinkie pie swear.mp3'
+    'swear': 'Pinkie Pie/pinkie pie swear.mp3',
+    'shoosh': 'Other/shoosh.mp3'
 };
 
 /* GENERAL APPLICATION STUFF */
