0e127f78617047d178250db5d0b7d487101f78be
Benjamin Leuckefeld <code@deratrox.de>
2017-01-27 18:30:51 +0100

    added roles command

---
+++ commands/utils/roles.js
@@ -0,0 +1,24 @@
+const roles = require('../../config/roles'),
+    table = require('text-table');
+
+exports.run = (bot, message, args) => {
+    let roleTable = [];
+
+    bot.server.roles.forEach(role => {
+        roleTable.push([role.name, role.id])
+    });
+
+    bot.respond(message, 'Liste aller Rollen des Servers:\n```' + table(roleTable, {hsep: '    '})+ '```');
+};
+
+exports.config = {
+    aliases: ['r'],
+    server: true,
+    role: roles.moderator
+};
+
+exports.help = {
+    name: 'roles',
+    description: 'Zeigt alle vorhandenen Rollen des Servers inklusive ihrer IDs an.',
+    usage: ['!roles']
+};
