07eec9c91ae8e0352e3119ab1fc4a344f78620eb
Benjamin Leuckefeld <code@deratrox.de>
2017-08-02 21:48:54 +0200

    add trusted group after 3 days

--- events/guild_member_update.js
+++ events/guild_member_update.js
@@ -12,7 +12,7 @@ exports.run = (oldMember, newMember) => {
     if(!bot.checkTrustedMember(oldMember) && bot.checkTrustedMember(newMember)){
         const joinedAt = moment(newMember.joinedAt);
 
-        if(moment().diff(moment(joinedAt).add(7, 'days')) < 0) {
+        if(moment().diff(moment(joinedAt).add(3, 'days')) < 0) {
             newMember.removeRole(roles.trusted);
         }
     }
