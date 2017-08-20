ce52a8ac3e2c2bdb36132a050bedac64842f2145
Benjamin Leuckefeld <code@deratrox.de>
2017-08-20 18:14:18 +0200

    added setExp command

---
+++ commands/mod/setexp.js
@@ -0,0 +1,54 @@
+const roles = require('../../config/roles'),
+    table = require('text-table');
+
+exports.run = (bot, message, args) => {
+    if (args.length < 2) {
+        bot.respond(message, 'dieser Befehl benötigt zusätzliche Parameter. Mehr unter `!help setexp`', true, 10);
+        return message.delete();
+    }
+
+    const target = bot.getGuildMemberFromArgs(message, args, 0);
+
+    if (target == null) {
+        bot.respond(message, `der Nutzer \`${args[0]}\` konnte nicht gefunden werden.`, true, 10);
+        return message.delete();
+    }
+
+    let exp = args[1];
+
+    if(isNaN(exp)) {
+        bot.respond(message, `der angegebene EXP-Wert ist keine Zahl`, true, 10);
+        return message.delete();
+    }
+
+    exp = parseInt(exp);
+
+    if(exp < 0) {
+        bot.respond(message, `der EXP-Wert muss über 0 sein.`, true, 10);
+        return message.delete();
+    }
+
+    bot.levels.setExp(target, exp, err => {
+        if(err) {
+            bot.log(`[Levels] SetExp error ${err}`);
+            bot.respond(message, `der EXP-Wert konnte nicht gesetzt werden, da ein Fehler aufgetreten ist.`, true, 10);
+            return message.delete();
+        }
+
+        bot.respond(message, `✅ EXP-Wert für ${target} von ${message.author} auf **${exp}** gesetzt. __Neues Level: ${bot.levels.getLevelFromXp(exp)}__`)
+        bot.log(`!setexp: ${message.author.tag} set exp for ${target.user.tag} to ${exp}.`);
+        message.delete();
+    })
+};
+
+exports.config = {
+    server: true,
+    role: roles.moderator,
+    trusted: false,
+};
+
+exports.help = {
+    name: 'setexp',
+    description: 'Setzt die EXP für einen Nutzer.',
+    usage: ['!setexp @Discord 500']
+};
--- modules/levels.js
+++ modules/levels.js
@@ -1,5 +1,11 @@
+const moment = require('moment');
+
+moment.locale('de');
+
 function Levels(bot) {
     this.bot = bot;
+
+    this.users = {};
 }
 
 Levels.prototype.getLevelExp = function (level) {
@@ -25,6 +31,14 @@ Levels.prototype.giveExp = function (message) {
     const key = `${this.bot.server.id}.Levels`;
     const field = message.author.id;
 
+    if(message.author.id in this.users) {
+        const timeoutEnd = this.users[message.author.id];
+
+        if(timeoutEnd.diff(moment()) > 0) {
+            return;
+        }
+    }
+
     let getCurrentExp = () => {
         this.bot.redis.hget(key, field, (err, result) => {
             if(err) {
@@ -44,12 +58,12 @@ Levels.prototype.giveExp = function (message) {
     let updateExp = currentExp => {
         const newExp = currentExp + this.getRandomExp();
 
-        this.bot.log(newExp);
-
         this.bot.redis.hset(key, field, newExp, err => {
             if(err) {
                 return this.bot.log(`[Levels] Could not giveExp (hset) ${err}`);
             }
+
+            this.users[message.author.id] = moment().add(1, 'm');
 
             const currentLevel = this.getLevelFromXp(currentExp);
             const newLevel = this.getLevelFromXp(newExp);
@@ -61,6 +75,19 @@ Levels.prototype.giveExp = function (message) {
     };
 
     getCurrentExp();
+};
+
+Levels.prototype.setExp = function (member, exp, callback) {
+    const key = `${this.bot.server.id}.Levels`;
+    const field = member.id;
+
+    this.bot.redis.hset(key, field, exp, err => {
+        if(err) {
+            return callback(err);
+        }
+
+        callback(false);
+    })
 };
 
 
