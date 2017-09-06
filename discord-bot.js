343d908ea03c24734b9c36e37f9e11b495c234dc
Benjamin Leuckefeld <code@deratrox.de>
2017-09-06 22:01:06 +0200

    removed spoiler/ database class

--- discord-bot.js
+++ discord-bot.js
@@ -3,7 +3,6 @@ let Discord = require('discord.js'),
     roles = require('./config/roles'),
     Twitter = require('./modules/twitter').Twitter,
     YouTube = require('./modules/youtube').YouTube,
-    Database = require('./modules/database').Database,
     Levels = require('./modules/levels').Levels,
     fs = require('fs'),
     exec = require('child_process').exec,
@@ -23,7 +22,6 @@ bot.events = new Discord.Collection();
 bot.config = config;
 
 bot.youtube = new YouTube(bot);
-bot.database = new Database(bot);
 bot.twitter = new Twitter(config.TWITTER_API, bot);
 bot.levels = new Levels(bot);
 
