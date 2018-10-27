fdbe841cd3c65063db324f874e701a090f11bfca
Benjamin Leuckefeld <code@deratrox.de>
2018-10-27 17:39:37 +0200

    add mlem random

--- commands/automessage/mlem.js
+++ commands/automessage/mlem.js
@@ -2,13 +2,19 @@ const roles = require('../../config/roles'),
     Discord = require('discord.js');
 
 exports.run = (bot, message, args) => {
+    function randomInt(low, high) {
+        return Math.floor(Math.random() * (high - low + 1) + low);
+    }
+
+    let file = `_mlem${randomInt(1, 3)}.gif`;
+
     let embed = new Discord.RichEmbed({
         image: {
-            url: `${bot.config.BASE_URL}/i/_mlem.gif`
+            url: `${bot.config.BASE_URL}/i/${file}`
         },
         description: '**Mlem o3o**',
         color: 0x399CE7
-    }).setFooter(`Der Chat wurde gemlemt von ${bot.server.members.get(message.author.id).displayName} | Art by n0nnny`);
+    }).setFooter(`Der Chat wurde gemlemt von ${bot.server.members.get(message.author.id).displayName}`);
 
     message.channel.send({embed});
 };
